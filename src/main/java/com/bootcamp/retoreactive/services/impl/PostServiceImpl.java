da4f39305e744fe05392bf0891c2537f99be3d4f
icruzadoe <ivanlibra28@gmail.com>
2022-03-06 15:14:58 -0500

    se agrego la validacion de que se comente en post con estado publicado

--- src/main/java/com/bootcamp/retoreactive/services/impl/PostServiceImpl.java
+++ src/main/java/com/bootcamp/retoreactive/services/impl/PostServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bootcamp.retoreactive.services.impl;
 
 import com.bootcamp.retoreactive.core.exception.PostException;
+import com.bootcamp.retoreactive.entities.Comment;
 import com.bootcamp.retoreactive.entities.Post;
 import com.bootcamp.retoreactive.repositories.BlogRepository;
 import com.bootcamp.retoreactive.repositories.PostRepository;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PostServiceImpl implements PostService {
@@ -23,9 +27,20 @@ public class PostServiceImpl implements PostService {
         return  this.blogRepository.findById(post.getBlogId()).doOnNext(b->{
                     System.out.println("doOnNext author = " + b);
                 })
-                .flatMap(b->
+                .flatMap(blog ->
                 {
-                    return b.getStatus().equals("activo") ? this.postRepository.save(post) : Mono.error(new PostException("El blog no esta activo"));
+                    if(blog.getStatus().equals("activo")){
+                        if(post.getStatus().equals("publicado") && post.getComments().size() > 0 ){
+                            return this.postRepository.save(post);
+                        }else{
+                            List<Comment> commentEmpty = new ArrayList<Comment>();
+                            post.setComments(commentEmpty);
+                            return this.postRepository.save(post);
+                        }
+                    }else
+                        return Mono.error(new PostException("El blog no esta activo"));
+
+                    //return b.getStatus().equals("activo") ? this.postRepository.save(post) : Mono.error(new PostException("El blog no esta activo"));
                 });
     }
 
